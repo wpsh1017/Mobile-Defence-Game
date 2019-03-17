9bd5c21abbc8d50c4d7f3f629a61f251271bc2f3
zenobreaker <wpsh6369@naver.com>
2019-03-18 02:08:04 +0900

    코루틴 적용

--- Mobile Defence Game/Assets/Scripts/BulletBehavior.cs
+++ Mobile Defence Game/Assets/Scripts/BulletBehavior.cs
@@ -9,7 +9,6 @@ public class BulletBehavior : MonoBehaviour {
     public BulletStat bulletStat { get; set; }
 
     public float activeTime = 3.0f;
-    public float spawnTime;
 
     public BulletBehavior()
     {
@@ -19,22 +18,25 @@ public class BulletBehavior : MonoBehaviour {
     public void Spawn()
     {
         gameObject.SetActive(true);
-        spawnTime = Time.time;
+    }
+    //오브젝트 풀링 기법과 함께 쓰면 효율적. 
+    private void OnEnable()
+    {
+        StartCoroutine(BulletInactive(activeTime));
     }
 
-	void Start () {
+    IEnumerator BulletInactive(float activeTime)
+    {
+        yield return new WaitForSeconds(activeTime);
+        gameObject.SetActive(false);
+    }
+
+    void Start () {
         Spawn();
 	}
 	
 	void Update () {
-        if (Time.time - spawnTime >= activeTime)
-        {
-            gameObject.SetActive(false);
-        }
-        else
-        {
             transform.Translate(Vector2.right * bulletStat.speed * Time.deltaTime);
-        }
 	}
 
     private void OnTriggerEnter2D(Collider2D other)
--- Mobile Defence Game/Assets/Scripts/CreateMonster.cs
+++ Mobile Defence Game/Assets/Scripts/CreateMonster.cs
@@ -11,49 +11,60 @@ public class CreateMonster : MonoBehaviour {
 
     private GameObject monsterPrefab;
 
-    private float lastSpawnTime;
     private int spawnCount = 0;
+    private IEnumerator coroutine;
 
     void Start () {
         monsterPrefab = monster1Prefab;
-        lastSpawnTime = Time.time;
+        coroutine = process();
+        StartCoroutine(coroutine);
 	}
-	
-	void Update () {
-		if(GameManager.instance.round <= GameManager.instance.totalRound)
+
+    void Create()
+    {
+        int index = Random.Range(0, 4);
+        GameObject respawnSpot = respawnSpots[index];
+        Instantiate(monsterPrefab, respawnSpot.transform.position, Quaternion.identity);
+        spawnCount += 1;
+        GameManager.instance.monsterAddCount++;
+    }
+
+	IEnumerator process()
+    {
+        while (true)
         {
-            float timeGap = Time.time - lastSpawnTime;
-            if(((spawnCount == 0 && timeGap > GameManager.instance.roundReadyTime) // 새 라운드가 시작
-                || timeGap > GameManager.instance.spawnTime)
-                && spawnCount < GameManager.instance.spawnNumber)
+            if (GameManager.instance.round > GameManager.instance.totalRound) StopCoroutine(coroutine);
+            if(spawnCount < GameManager.instance.spawnNumber)
             {
-                lastSpawnTime = Time.time;
-                int index = Random.Range(0, 4);
-                GameObject respawnSpot = respawnSpots[index];
-                Instantiate(monsterPrefab, respawnSpot.transform.position, Quaternion.identity);
-                spawnCount += 1;
-                GameManager.instance.monsterAddCount++;
+                Create();
             }
             if(spawnCount == GameManager.instance.spawnNumber &&
-               GameObject.FindGameObjectWithTag("Monster") == null)
+                GameObject.FindGameObjectWithTag("Monster") == null)
             {
                 if(GameManager.instance.totalRound == GameManager.instance.round)
                 {
                     GameManager.instance.gameClear();
                     GameManager.instance.round += 1;
-                    return;
                 }
-                GameManager.instance.clearRound();
-                spawnCount = 0;
-                lastSpawnTime = Time.time;
-
-                if(GameManager.instance.round == 4)
+                else
                 {
-                    monsterPrefab = monster2Prefab;
-                    GameManager.instance.spawnTime = 2.0f;
-                    GameManager.instance.spawnNumber = 10;
+                    GameManager.instance.clearRound();
+                    spawnCount = 0;
+
+                    if (GameManager.instance.round == 4)
+                    {
+                        monsterPrefab = monster2Prefab;
+                        GameManager.instance.spawnTime = 2.0f;
+                        GameManager.instance.spawnNumber = 10;
+                    }
                 }
             }
+            if (spawnCount == 0) yield return new WaitForSeconds(GameManager.instance.roundReadyTime);
+            else yield return new WaitForSeconds(GameManager.instance.spawnTime);
         }
-	}
+    }
+
+    void Update()
+    {
+    }
 }
